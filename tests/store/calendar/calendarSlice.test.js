5ec6ec7289d2706348deb66d6826ccfd920ca185
Francisco Pereira <francisco.pereira16@inacapmail.cl>
2024-01-24 13:12:37 -0300

    Add calendarSlice tests

---
+++ tests/store/calendar/calendarSlice.test.js
@@ -0,0 +1,74 @@
+import { calendarSlice, onAddNewEvent, onSetActiveEvent, onUpdateEvent,onDeleteEvent, onLoadEvents, onLogoutCalendar } from "../../../src/store/calendar/calendarSlice"
+import { calendarWithActiveEventState, calendarWithEventsState, events, initialState } from "../../fixtures/calendarStates"
+
+
+describe('Pruebas en CalendarSlice', () => { 
+
+  test('debe de regresar el estado por defecto',  () => { 
+
+    const state = calendarSlice.getInitialState()
+    expect( state ).toEqual(initialState)
+
+  })
+
+  test('onSetActiveEvent debe de activar el evento', () => { 
+
+    const state = calendarSlice.reducer( calendarWithEventsState, onSetActiveEvent( events[0] ) )
+    expect( state.activeEvent ).toEqual( events[0] )
+    
+  })
+
+  test('onAddNewEvent debe de agregar el evento ',  () => { 
+
+    const newEvent = {
+      id: '3',
+      start: new Date('2024-12-23 13:00:00'),
+      end: new Date('2024-12-23 14:00:00'),
+      title: 'Event 3',
+      notes : 'Test notes 3',
+    };
+
+    const state = calendarSlice.reducer( calendarWithEventsState, onAddNewEvent( newEvent ) )
+    expect( state.events ).toEqual( [...events, newEvent] )
+
+  })
+
+  test('onUpdateEvent debe de actualizar el evento', () => { 
+
+    const updateEvent = {
+      id: '1',
+      start: new Date('2024-12-23 13:00:00'),
+      end: new Date('2024-12-23 14:00:00'),
+      title: 'Event 3 update',
+      notes : 'Test notes 3 update',
+    };
+
+    const state = calendarSlice.reducer( calendarWithEventsState, onUpdateEvent( updateEvent ) )
+    expect( state.events).toContain( updateEvent )
+
+  })
+
+  test('onDeleteEvent debe de borrar el evento activo', () => { 
+
+    const state = calendarSlice.reducer( calendarWithActiveEventState, onDeleteEvent() )
+    expect( state.activeEvent ).toBe( null )
+    expect( state.events ).not.toContain( events[0]) // el evento 1 ya no debe de existir en el array de eventos
+
+  })
+
+  test('onLoadEvents debe de establecer los eventos', () => { 
+      
+      const state = calendarSlice.reducer( initialState, onLoadEvents( events ) )
+      expect( state.isLoadingEvents ).toBeFalsy()
+      expect( state.events ).toEqual( events )
+
+      // debe de agregar los eventos al estado si ya existen eventos en el estado 
+      const newState = calendarSlice.reducer( state, onLoadEvents( events ) )
+      expect( state.events.length ).toBe( events.length )
+  })
+
+  test('onLogoutCalendar debe de limpiar el estado', () => { 
+    const state = calendarSlice.reducer( calendarWithEventsState, onLogoutCalendar() )
+    expect( state ).toEqual( initialState )
+  })
+})
