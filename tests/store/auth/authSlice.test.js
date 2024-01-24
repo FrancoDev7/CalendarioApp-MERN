a19713fb2ea04e27a1b8a48435d3183e60cf141d
Francisco Pereira <francisco.pereira16@inacapmail.cl>
2024-01-24 12:41:07 -0300

    Add authentication tests to authSlice

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,13 +1,54 @@
-import { authSlice } from "../../../src/store/auth/authSlice"
-import { initialState } from "../../fixtures/authStates"
+import { authSlice, onLogin, onLogout,clearErrorMessage } from "../../../src/store/auth/authSlice"
+import { authenticatedState, initialState } from "../../fixtures/authStates"
+import { testUserCredentials } from "../../fixtures/testUser"
 
 describe('Pruebas en authSlice ', () => { 
 
   test('debe de regresar el estado inicial', () => { 
-
     expect( authSlice.getInitialState() ).toEqual(initialState)
-    
 
-  
   })
+
+  test('debe de realizar el login', () => {
+
+    const state = authSlice.reducer(initialState, onLogin( testUserCredentials ))
+    expect( state ).toEqual({
+      status: 'authenticated',
+      user: testUserCredentials,
+      errorMessage:  undefined
+    })
+    
+  })
+
+  test('debe de realizar el logout', () => {
+
+    const state = authSlice.reducer( authenticatedState, onLogout() )
+    expect( state ).toEqual({
+      status: 'not-authenticated',
+      user: {},
+      errorMessage:  undefined
+    })
+  })
+
+  test('debe de realizar el logout', () => {
+    const errorMessage = 'credenciales incorrectas'
+    
+    const state = authSlice.reducer( authenticatedState, onLogout(errorMessage) )
+    // console.log(state)
+    expect( state ).toEqual({
+      status: 'not-authenticated',
+      user: {},
+      errorMessage:  errorMessage
+    })
+  })
+
+  test('debe de limpiar el mensaje de error', () => { 
+
+    const errorMessage = 'credenciales incorrectas'
+    const state = authSlice.reducer( authenticatedState, onLogout(errorMessage) ) 
+    const newState = authSlice.reducer( state, clearErrorMessage() )
+
+    expect( newState.errorMessage ).toBe(undefined)
+  })
+
 })
